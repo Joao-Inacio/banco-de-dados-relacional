773f44e1c435356cedff97d90a273faef190cc04
Joao-Inacio <joaoinacio206@protonmail.com>
2023-09-20 10:46:47 -0300

    Códigos DELETE para excluir dados no banco de dados

---
+++ queries-exemplo/remocoes.sql
@@ -0,0 +1,8 @@
+-- Active: 1691416914754@@127.0.0.1@3306@bd_interprise
+DELETE FROM pessoa WHERE id_pessoa = 1;
+DELETE FROM pessoa WHERE id_pessoa = 3;
+DELETE FROM pessoa;
+DELETE FROM funcionario;
+
+TRUNCATE TABLE pessoa;
+TRUNCATE TABLE funcionario;
