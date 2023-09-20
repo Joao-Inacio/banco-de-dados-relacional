e9fe60ec7bea97eb64a7a3a70c3edee09d353fa9
Joao-Inacio <joaoinacio206@protonmail.com>
2023-09-20 10:44:15 -0300

    Códigos UPDATE para atualizar os dados

---
+++ queries-exemplo/atualizacoes.sql
@@ -0,0 +1,8 @@
+-- Active: 1691416914754@@127.0.0.1@3306@bd_interprise
+# Atualizando o Nome
+UPDATE pessoa SET nome = "Will"
+WHERE id_pessoa = 3;
+
+# Atualizando o preço da tabela produto
+UPDATE produto SET preco = 49
+WHERE id_produto = 1;
